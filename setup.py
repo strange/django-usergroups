6441c22beea60472ea45de83b7d4cda66a76a010
Gustaf Sjöberg <gs@distrop.com>
2009-09-13 20:56:14 +0200

    Added setuptools files.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='usergroups', version='0.1',
+      description='Simple reusable Django group app',
+      author='Gustaf Sjöberg', author_email='gs@distrop.com',
+      packages=['usergroups'],
+      zip_safe=False)
